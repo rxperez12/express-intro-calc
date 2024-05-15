2e71d13477c2bb418c114d990708a5cdc3937fe5
Ricardo Perez <rxperez12@gmail.com>
2024-05-15 11:59:28 -0700

    finished mean endpoint

--- calc/app.js
+++ calc/app.js
@@ -1,22 +1,48 @@
 /** Simple demo Express app. */
 
 import express from "express";
+import { findMean, findMedian, findMode } from './stats.js';
 const app = express();
 
 // useful error class to throw
-import { NotFoundError } from "./expressError.js";
+import { NotFoundError, BadRequestError } from "./expressError.js";
+
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
+app.use(express.json());                           // process JSON data
+app.use(express.urlencoded());                     // process trad form data
+
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
+  const nums = req.query.nums;
+  let mean;
+  const numbers = nums.split(',').map(str => Number(str));
+  const invalidNums = numbers.filter(num => isNaN(num));
 
+  if (nums === '' || invalidNums.length > 0) {
+    throw new BadRequestError;
+  } else {
+    mean = findMean(numbers);
+  }
+
+  return res.json({
+    response: { operation: 'mean', value: mean }
+  });
+
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
-
+app.get("/median", function (req, rest) {
+  const nums = req.query.nums;
+  console.log(nums);
+});
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get('/mode', function (req, res) {
 
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
